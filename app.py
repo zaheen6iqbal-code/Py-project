f95ad4c98d30ef463cab3a1125cc49e4287d6236
zaheen6iqbal-code <zaheen6iqbal@gmail.com>
2025-11-21 20:52:39 +0500

    My first Update

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pickle 
+
+
+## to load the model and label encoder
+## rb = run binary
+model = pickle.load(open("car_model.pkl","rb"))
+le = pickle.load(open("label_encoder.pkl","rb"))
+
+st.title("Car Price Prediction app")
+
+#dropdown for car model (from labelencoder classes)
+
+car_model = st.selectbox("Select Car Model", le.classes_)
+
+#user input
+
+mileage = st.number_input("Enter Mileage (in miles)", min_value=0)
+age = st.slider("Car age (year)",0,8)
+
+## selected car model to encode value for prediction
+
+encoded_model = le.transform([car_model])[0]
+
+if st.button("Predict Price"):
+    input_data = [[encoded_model,mileage,age]]
+    predicted_price = model.predict(input_data)
+    st.success(f"Estimated selling price: {predicted_price[0]}")
+
+
+
+
+
